7565807c7d85d20e92712ff95f12fdf3c68d487b
Vincent Son <107471539+szw0407@users.noreply.github.com>
2024-01-26 02:40:40 +0800

    Refactor main module and update start script

--- main/__init__.py
+++ main/__init__.py
@@ -1,1 +1,8 @@
 from .app import *
+import uvicorn, sys
+
+def main():
+    # read --host and --port from command line, e.g. `python main.py --host 127.0.0.1 --port 8000`
+    host: str = sys.argv[sys.argv.index("--host") + 1] if "--host" in sys.argv else "127.0.0.1"
+    port: str = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else "8000"
+    uvicorn.run(app, host=host, port=int(port))
--- main/__main__.py
+++ main/__main__.py
@@ -1,12 +1,5 @@
-from .app import *
-import uvicorn, sys
+from . import main
 
-
-def main():
-    # read --host and --port from command line, e.g. `python main.py --host 127.0.0.1 --port 8000`
-    host: str = sys.argv[sys.argv.index("--host") + 1] if "--host" in sys.argv else "127.0.0.1"
-    port: str = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else "8000"
-    uvicorn.run(app, host=host, port=int(port))
 
 if __name__ == "__main__":
     main()
